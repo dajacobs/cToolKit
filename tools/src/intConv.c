854b83acc98be061ac9bad3ab84c14981265bc30
dajacobs <dajacobs.tab@gmail.com>
2015-09-17 09:36:09 -0500

    conversions func

---
+++ tools/src/intConv.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Main function: examples of int conversions.
+int main(void) {
+	printf("%d\n", 455);
+	printf("%i\n", 455);
+	printf("%d\n", +455);
+	printf("%hd\n", 32000);
+	printf("%ld\n", 200000000L);
+	printf("%o\n", 455);
+	printf("%u\n", 455);
+	printf("%u\n", -455);
+	printf("%x\n", 455);
+	printf("%X\n", 455);
+}
