7e88781ad2591681022245a40468501db69b690b
dajacobs <dajacobs.tab@gmail.com>
2015-09-22 16:00:34 -0500

    main func

---
+++ tools/src/shuffle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CARDS 52
+#define FACES 13
+
+// Card structure
+struct card {
+	const char *face;
+	const char *suit;
+};
+
+typedef struct card Card;
+
+// Prototypes
+void fillDeck(Card * const wDeck, const char * wFace[], const char * wSuit[]);
+void shuffle(Card * const wDeck);
+void deal(const Card * const wDeck);
+
+// Main function
+int main(void) {
+	Card deck[CARDS];
+
+	const char *face[] = {"Ace", "Deuce", "Three", "Four", "Fiver", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+	const char *suit[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+	srand(time(NULL));
+
+	fillDeck(deck, face, suit);
+	shuffle(deck);
+	deal(deck);
+}
