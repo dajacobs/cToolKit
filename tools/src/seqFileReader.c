b9d207b62c284226bc36e75e5147e54758a059a8
dajacobs <dajacobs.tab@gmail.com>
2015-09-24 09:57:41 -0500

    main func

--- tools/src/seqFileReader.c
+++ tools/src/seqFileReader.c
@@ -2,8 +2,30 @@
 
 // clientData structure
 struct clientData {
-	usigned int acctNum;
+	unsigned int acctNum;
 	char lastName[15];
 	char firstName[10];
 	double balance;
 };
+
+// Main function
+int main(void) {
+	FILE *cfPtr;
+
+	// Error message
+	if((cfPtr = fopen("credits.txt", "rb")) == NULL) {
+		puts("File could not be opened.");
+	} else {
+		printf("%-6s%-16s%-11s%10s\n", "Acct", "Last Name", "First Name", "Balance");
+
+		while(!feof(cfPtr)) {
+			struct clientData client = {0, "", "", 0.0};
+			int result = fread(&client, sizeof(struct clientData), 1, cfPtr);
+
+			if(result != 0 && client.acctNum != 0) {
+				printf("%-6d%-16s%-11s%10.2f\n", client.acctNum, client.lastName, client.firstName, client.balance);
+			}
+		}
+		fclose(cfPtr);
+	}
+}
