4ffea068c1556e99a03d8dd5c6feb05c1de79650
dajacobs <dajacobs.tab@gmail.com>
2015-10-15 08:30:55 -0500

    update

--- math/src/vector.cpp
+++ math/src/vector.cpp
@@ -13,6 +13,23 @@ int main() {
 	vector<int> integers1(7);
 	vector<int> integers2(10);
 
+	// Print size of first vector
 	cout << "Size of vector one is: " << integers1.size() << "\nvector after intialization:" << endl;
 	outputVector(integers1);
+	// Print size of second vector
+	cout << "\nSize of vector two is " << integers2.size() << "\nvector after intialization:" << endl;
+	outputVector(integers2);
+
+	// Print first and second integers
+	cout << "\nEnter 17 integers:" << endl;
+	
+	// Input vectors
+	inputVector(integers1);
+	inputVector(integers2);
+
+	// Print vectors after input
+	cout << "\nAfter input, the vectors contain:\n" << "first integer:" << endl;
+	outputVector(integers1);
+	cout << "second vector:" << endl;
+	outputVector(integers2);
 }
