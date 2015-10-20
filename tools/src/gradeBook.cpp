246cb1bc46503a9109c0960875ac4b29ca6bce26
dajacobs <dajacobs.tab@gmail.com>
2015-10-20 08:20:38 -0500

    construct funcs

--- tools/src/gradeBook.cpp
+++ tools/src/gradeBook.cpp
@@ -6,6 +6,15 @@ class GradeBook {
 public:
 	// Constructor
 	explicit GradeBook(string name):courseName(name) {
-			
+
+	}
+	void setCourseName(string name) {
+		courseName = name;
+	}
+	string getCourseName() {
+		return courseName;
+	}
+	void displayMessage() {
+		cout << "Welcome to the grade book for \n" << getCourseName() << "!" << endl;
 	}
 }
