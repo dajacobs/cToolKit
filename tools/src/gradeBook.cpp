ffde69e1d6d0705e1c765a4f5f250981bcd6b966
dajacobs <dajacobs.tab@gmail.com>
2015-10-20 08:27:29 -0500

    main func

--- tools/src/gradeBook.cpp
+++ tools/src/gradeBook.cpp
@@ -17,4 +17,13 @@ public:
 	void displayMessage() {
 		cout << "Welcome to the grade book for \n" << getCourseName() << "!" << endl;
 	}
+private:
+	string courseName;
+};
+// Main function
+int main() {
+	GradeBook gradeBook1("C++ Programming Class");
+	GradeBook gradeBook2("C++ Programming Lab");
+
+	cout << "Course One: " << gradeBook1.getCourseName() << "\nCourse Two: " << gradeBook2.getCourseName() << endl;
 }
