11c153c55fdf4ce910b5ddd7d4ad9eb8ea49c47b
dajacobs <dajacobs.tab@gmail.com>
2015-10-24 20:52:54 -0500

    time set func

--- tools/src/time.h
+++ tools/src/time.h
@@ -4,4 +4,10 @@
 // Time class
 class Time {
 	explicit Time(int = 0, int = 0, int = 0);
+
+	// Time setting functions
+	void setTime(int, int, int);
+	void setHour(int);
+	void setMinute(int);
+	void setSecond(int);
 }
