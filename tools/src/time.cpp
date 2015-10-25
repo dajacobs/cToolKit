b0c455625f54995bd666ff9b7b819b1eeec29c3f
dajacobs <dajacobs.tab@gmail.com>
2015-10-24 21:10:47 -0500

    set time universal

--- tools/src/time.cpp
+++ tools/src/time.cpp
@@ -8,3 +8,10 @@ using namespace std;
 Time::Time(int hour, int minute, int second) {
 	setTime(hour, minute, second);
 }
+
+// Setting time with universal format
+void Time::setTime(int h, int m, int s) {
+	setHour(h);
+	setMinute(m);
+	setSecond(s);
+}
