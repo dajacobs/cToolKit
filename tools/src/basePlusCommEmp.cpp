feada00287add4cc3e786d8fe26b1bfa9a24555b
dajacobs <dajacobs.tab@gmail.com>
2015-11-03 11:49:33 -0600

    base salary setter

--- tools/src/basePlusCommEmp.cpp
+++ tools/src/basePlusCommEmp.cpp
@@ -7,3 +7,11 @@ using namespace std;
 basePlusCommEmploy::basePlusCommEmploy(const string &first, const string &last, const string &ssn, double sales, double rate, double salary):commissionEmployee(first, last, ssn, sales, rate) {
 	setBaseSal(salary);
 }
+// Setter for base salary
+void basePlusCommEmploy::setBaseSal(double salary) {
+	if(salary >= 0.0) {
+		baseSalary = salary;
+	} else {
+		throw invalid_arguement("Salary must be >= 0.0");
+	}
+}
