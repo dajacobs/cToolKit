45df3c5df53a36d48ae33dd39156445ec902863a
dajacobs <dajacobs.tab@gmail.com>
2015-11-03 11:21:04 -0600

    base commission employee class: public

---
+++ tools/src/baseCommEmploy.h
@@ -0,0 +1,18 @@
+#ifndef BASEPLUS_H
+#define BASEPLUS_H
+#include <string>
+#include "commissionEmployee.h"
+
+class BaseCommEmploy : public commissionEmployee {
+public:
+	// Constructor for base commission employee
+	BaseCommEmploy(const std::string &, const std::string &, const std::string &, double = 0.0, double = 0.0, double = 0.0);
+	// Setter for base salary
+	void setBaseSal(double);
+	// Getter for base salary
+	double getBaseSal() const;
+	// Earnings
+	double earnings() const;
+	// Print object
+	void print() const;
+}
