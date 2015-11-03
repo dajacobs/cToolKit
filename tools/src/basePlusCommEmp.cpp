8a9278e363c6cde7565d37a3f3b02beb243e1976
dajacobs <dajacobs.tab@gmail.com>
2015-11-03 11:54:59 -0600

    edit

--- tools/src/basePlusCommEmp.cpp
+++ tools/src/basePlusCommEmp.cpp
@@ -19,7 +19,7 @@ void basePlusCommEmploy::setBaseSal(double salary) {
 double basePlusCommEmploy::getBaseSal() const {
 	return baseSalary;
 }
-// Calculate earnings
+// Getter for earnings
 double basePlusCommEmploy::earnings() const {
 	return getBaseSal() + commissionEmployee::earnings();
 }
