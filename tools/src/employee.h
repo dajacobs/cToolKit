11f0351a01fa5ce02451850c3fc5c39f05ad06d2
dajacobs <dajacobs.tab@gmail.com>
2015-11-05 11:42:57 -0600

    setters/getters

--- tools/src/employee.h
+++ tools/src/employee.h
@@ -8,4 +8,13 @@ public:
 	Employee(const std::string &, const std::string &, const std::string &);
 	// Employee destructor
 	virtual ~Employee() {}
+
+	// Setters
+	void setFirstName(const std::string &);
+	void setLastName(const std::string &);
+	void setSocialSecurityNumber(const std::string &);
+	// Getter
+	std::string getFirstName() const;
+	std::string getLastName() const;
+	std::string getSocialSecurityNumber() const;
 }
