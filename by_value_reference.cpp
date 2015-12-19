82cc34521e1da9eb3f1b9ec2f7ad99ae40fe0537
dajacobs <dajacobs.tab@gmail.com>
2015-12-18 22:29:01 -0600

    main func - by value

--- by_value_reference.cpp
+++ by_value_reference.cpp
@@ -4,3 +4,13 @@ using namespace std;
 // Prototypes 
 int squareByValue(int);
 void squareByRerence(int &);
+
+// Main function
+int main()
+{
+	// Square by value demo
+	int x = 2;
+	cout << "x = " << x << " before squareByValue\n";
+	cout << "Value returned by squareByValue: " << squareByValue(x) << endl;
+	cout << "x = " << x << " after squareByValue\n" << endl;
+}
