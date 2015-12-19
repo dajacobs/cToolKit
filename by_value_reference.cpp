69858d873fd3bcfdc2a6097d9e415d56d8b3b469
dajacobs <dajacobs.tab@gmail.com>
2015-12-18 22:32:58 -0600

    main func - by reference

--- by_value_reference.cpp
+++ by_value_reference.cpp
@@ -13,4 +13,10 @@ int main()
 	cout << "x = " << x << " before squareByValue\n";
 	cout << "Value returned by squareByValue: " << squareByValue(x) << endl;
 	cout << "x = " << x << " after squareByValue\n" << endl;
+
+	// Square by reference demo
+	int z = 4;
+	cout << "z = " << z << << " before squareByRerence" << endl;\
+	squareByRerence(z);	
+	cout << "z = " << z << << " after squareByRerence" << endl;
 }
