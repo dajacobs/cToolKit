78ab5870826c03226e0cfb5086ceb7fc49053878
dajacobs <dajacobs.tab@gmail.com>
2015-12-18 21:42:50 -0600

    main func

--- math/src/volume_of_cube.cpp
+++ math/src/volume_of_cube.cpp
@@ -8,3 +8,15 @@ inline double cube(const double side)
 {
 	return side * side * side;
 }
+// Main function
+int main()
+{
+	double sideValue;
+	for(int i = 1; i <= 3; i++)
+	{
+		cout << "\nEnter the side length of your cube: ";
+		cin >> sideValue;
+
+		cout << "Volume of the cube with side" << sideValue << " is " << cube(sideValue) << endl; 
+	}
+}
